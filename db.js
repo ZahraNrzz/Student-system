a33ff691e71a5e9a095a23210cfc9293cb36a4e1
ZahraNrzz <zahra.nrz.z4@gmail.com>
2025-05-21 23:11:26 +0330

    add db

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017/';
-const client = new MongoClient(url);
-const dbName = 'myDatabase';
-async function connectToDatabase() {
-    try {
-        await client.connect();
-        console.log('Connected successfully to MongoDB');
-        const db = client.db(dbName);
-        return db;
-    } catch (err) {
-        console.error(err);
-        throw new Error('Failed to connect to MongoDB');
-    }
-}
-module.exports = { connectToDatabase, client };
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/StudentSystemDB');
+    console.log('✅ Connected to MongoDB via Mongoose');
+  } catch (err) {
+    console.error('❌ Database connection failed:', err.message);
+    throw err;
+  }
+};
+module.exports = connectToDatabase;
+
+// const { MongoClient } = require('mongodb');
