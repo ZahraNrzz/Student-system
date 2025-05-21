fd6d97afdfdb07e8cec4523ab9205864fee297a6
ZahraNrzz <zahra.nrz.z4@gmail.com>
2025-05-21 23:27:54 +0330

    Update db

--- db.js
+++ db.js
@@ -12,3 +12,18 @@ const connectToDatabase = async () => {
 module.exports = connectToDatabase;
 
 // const { MongoClient } = require('mongodb');
+// const url = 'mongodb://localhost:27017/';
+// const client = new MongoClient(url);
+// const dbName = 'myDatabase';
+// async function connectToDatabase() {
+//     try {
+//         await client.connect();
+//         console.log('Connected successfully to MongoDB');
+//         const db = client.db(dbName);
+//         return db;
+//     } catch (err) {
+//         console.error(err);
+//         throw new Error('Failed to connect to MongoDB');
+//     }
+// }
+// module.exports = { connectToDatabase, client };
